83d727b416a2e07b483283b3f6235d425d9b6edc
prabhat shankar <prabhat4686@gmail.com>
2025-01-09 07:03:32 +0530

    new env var added

--- api/index.py
+++ api/index.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World after cache clear!'
+    return 'Hello, World after cache clear again!'
 
 @app.route('/about')
 def about():
