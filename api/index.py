62953915207a949e836e95f67432e7396ca1edfa
prabhat shankar <prabhat4686@gmail.com>
2025-01-09 06:17:51 +0530

    updated indexpy

--- api/index.py
+++ api/index.py
@@ -1,20 +1,11 @@
-# Flask application for the chatbot
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def home_flask():
-    return 'Hello, flask home!'
-
-@app.route('/home')
 def home():
-    return 'Hello, World to the LLM Chatbot app!'
+    return 'Hello, World!'
 
 @app.route('/about')
 def about():
-    return 'Trying the about route!'
-
-# # Ensure the app runs locally for testing
-# if __name__ == "__main__":
-#     app.run(debug=True)
+    return 'About'
